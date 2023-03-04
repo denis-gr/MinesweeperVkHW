a00ba2d36b508e75605f66e0be89930616e0f383
denis-gr <qwertyuiop852002058@yandex.com>
2023-03-04 18:58:23 +0000

    commit

---
+++ Minesweeper/src/App.vue
@@ -0,0 +1,51 @@
+<script setup>
+</script>
+
+<template>
+  <header>
+    
+  </header>
+
+  <main>
+
+    <div class="sprite sprite-table-1"></div>
+    <div class="sprite sprite-table-2"></div>
+    <div class="sprite sprite-table-3"></div>
+    <div class="sprite sprite-table-4"></div>
+    <div class="sprite sprite-table-5"></div>
+    <div class="sprite sprite-table-6"></div>
+    <div class="sprite sprite-table-7"></div>
+    <div class="sprite sprite-table-8"></div>
+    <div class="sprite sprite-table-9"></div>
+    <div class="sprite sprite-table-0"></div>
+
+    <div class="sprite sprite-field-1"></div>
+    <div class="sprite sprite-field-2"></div>
+    <div class="sprite sprite-field-3"></div>
+    <div class="sprite sprite-field-4"></div>
+    <div class="sprite sprite-field-5"></div>
+    <div class="sprite sprite-field-6"></div>
+    <div class="sprite sprite-field-7"></div>
+    <div class="sprite sprite-field-8"></div>
+
+    <div class="sprite sprite-field-blank"></div>
+    <div class="sprite sprite-field-choosen"></div>
+    <div class="sprite sprite-field-flag"></div>
+    <div class="sprite sprite-field-question"></div>
+    <div class="sprite sprite-field-question-choosen"></div>
+    <div class="sprite sprite-field-mine"></div>
+    <div class="sprite sprite-field-mine-choosen"></div> 
+    <div class="sprite sprite-field-mine-cross"></div>
+    
+    <div class="sprite sprite-face-smile"></div>
+    <div class="sprite sprite-face-smile-choosen"></div>
+    <div class="sprite sprite-face-O"></div>
+    <div class="sprite sprite-face-glasses"></div>
+    <div class="sprite sprite-face-dead"></div>
+
+
+  </main>
+</template>
+
+<style scoped>
+</style>
