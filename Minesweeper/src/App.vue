97c361b6974535341abe0b0aa42a700362c1e48d
denis-gr <qwertyuiop852002058@yandex.com>
2023-03-05 18:56:20 +0000

    ююю

--- Minesweeper/src/App.vue
+++ Minesweeper/src/App.vue
@@ -1,4 +1,48 @@
-<script setup>
+<script>
+export default {
+  created() {
+    this.initField();
+  },
+  data () {
+    return {
+      flags: new Array(16).fill(0).map(() => new Array(16).fill(false)),
+      choosen: new Array(16).fill(0).map(() => new Array(16).fill(true)),
+      questions: new Array(16).fill(0).map(() => new Array(16).fill(true)),
+      table: new Array(16).fill(0).map(() => new Array(16).fill(1)),
+      mines: new Array(16).fill(0).map(() => new Array(16).fill(true)),
+    }
+  },
+  methods: {
+    getNeighboringMines(y, x) {
+      const moves = [[1, 1], [1, 0], [0, 1], [-1, -1], [-1, 0], [0, -1]];
+      return moves.filter(([x, y]) => this.mines[y][x]);
+    },
+    initField() {
+      const bombs = 16;
+      const cols = 16;
+      const rows = 16;
+      const size = rows * cols;
+      const grid = [];
+      /*for (let i = 0; i < size; i += 1) {
+        grid.push({
+          hasBomb: false,
+          isOpen: false,
+          hasFlag: false,
+          bombCount: 0,
+          neighborhood: null,
+        });
+      }*/
+      while (bombs > 0) {
+        const num = Math.floor(Math.random() * size);
+        console.log(1, [Math.floor(num / 16)], [num % 16])
+        if (this.mines[Math.floor(num / 16)][num % 16] == false) {
+          bombs -= 1;
+          this.mines[num / 16][num % 16] = true;
+        }
+      }
+    }
+  },
+}
 </script>
 
 <template>
@@ -7,8 +51,27 @@
   </header>
 
   <main>
+    <div class="game">
+      <div class="score"></div>
+      <div class="field">
+        <div class="row" v-for="row, y in table">
+          <div class="ceil" v-for="ceil, x in row">
+            <div class="sprite sprite-field-flag" v-if="flags[y][x]"></div>
+            <div class="sprite sprite-field-mine" v-else-if="choosen[y][x] && mines[y][x]"></div>
+            <div class="sprite sprite-field-question-choosen" v-else-if="choosen[y][x] && questions[y][x]"></div>
+            <div class="sprite sprite-field-question" v-else-if="!choosen[y][x] && questions[y][x]"></div>
+            <div class="sprite sprite-field-blank" v-else-if="!choosen[y][x]"></div>
+            <div class="sprite" :class="'sprite-field-'+getNeighboringMines[y][x]" v-else></div>
 
-    <div class="sprite sprite-table-1"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+
+
+    <!--div class="sprite sprite-table-1"></div>
     <div class="sprite sprite-table-2"></div>
     <div class="sprite sprite-table-3"></div>
     <div class="sprite sprite-table-4"></div>
@@ -41,11 +104,20 @@
     <div class="sprite sprite-face-smile-choosen"></div>
     <div class="sprite sprite-face-O"></div>
     <div class="sprite sprite-face-glasses"></div>
-    <div class="sprite sprite-face-dead"></div>
+    <div class="sprite sprite-face-dead"></div-->
 
 
   </main>
 </template>
 
 <style scoped>
+.game {
+  border: 10px solid #a5a5a5;
+  width: 256px;
+  height: 256px;
+
+}
+.row {
+  width: 256px;
+}
 </style>
